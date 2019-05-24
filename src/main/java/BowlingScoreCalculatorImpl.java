127b80f33c822061d8eaee0eecfdc2aa53c4c4ef
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2019-05-24 04:08:22 +0200

    Refactoring score methode and add conditionals for Spare

--- src/main/java/BowlingScoreCalculatorImpl.java
+++ src/main/java/BowlingScoreCalculatorImpl.java
@@ -1,12 +1,26 @@
 public class BowlingScoreCalculatorImpl implements BowlingScoreCalculator {
 
-    private int score = 0;
+    private int roll = 0;
+    private int[] rolls = new int[21];
 
     public void roll(int pins) {
-        score += pins;
+        rolls[roll++] = pins;
     }
 
     public int score() {
+        int score = 0;
+        int index = 0;
+        for (int frame = 0; frame < 10; frame++){
+           if (rolls[index] + rolls[index + 1] == 10){
+               score += 10 + rolls[index + 2];
+               index += 2;
+           }
+           else {
+               score += rolls[index] + rolls[index + 1];
+               index++;
+           }
+        }
+
         return score;
     }
 }
