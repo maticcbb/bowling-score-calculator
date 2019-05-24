5beedfed5e62431e619d1823400707d63d36395a
Mateusz Mielcarz <m.mielcarz93@gmail.com>
2019-05-24 05:05:59 +0200

    Create test for methode isFinished() checking that game ends

--- src/test/java/BowlingScoreCalculatorImplTest.java
+++ src/test/java/BowlingScoreCalculatorImplTest.java
@@ -20,45 +20,77 @@ public class BowlingScoreCalculatorImplTest {
 
     @Test
     void canScoreZeroPoints() {
+        // When
         roll( 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
-        assertThat(bowlingCalculator.score()).isEqualTo(0);
 
+        // Then
+        assertThat(bowlingCalculator.score()).isEqualTo(0);
     }
 
     @Test
     void canScoreGameOfOnes() {
+        // When
         roll(1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1);
+
+        // Then
         assertThat(bowlingCalculator.score()).isEqualTo(20);
     }
 
     @Test
     void canScoreSpareFollowedByFive() {
+        // When
         roll(5,5, 5,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+
+
+        // Then
         assertThat(bowlingCalculator.score()).isEqualTo(20);
     }
 
     @Test
     void canScoreStrikeFollowedByThree() {
+        // When
         roll(10, 3,3, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
+
+        // Then
         assertThat(bowlingCalculator.score()).isEqualTo(22);
     }
 
     @Test
     void canScoreAllStrikes() {
+        // When
         roll(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
+
+        // Then
         assertThat(bowlingCalculator.score()).isEqualTo(300);
     }
 
     @Test
     void canCalculateRounds() {
+        // When
         roll(10, 3,3, 0,0, 0,0, 0,0, 10, 0,0, 3,2, 10, 0,0);
+
+        // Then
         assertThat(bowlingCalculator.round()).isEqualTo(10);
     }
 
     @Test
     void canCalculateActualRound() {
+        // When
         roll(10, 3,3, 10, 0,0, 3,2, 10, 0,0);
+
+        // Then
         assertThat(bowlingCalculator.round()).isEqualTo(8);
+    }
+
+    @Test
+    public void shouldReturnTrue()  {
+        // When
+        roll(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
+
+
+        // Then
+        assertThat(bowlingCalculator.isFinished()).isEqualTo(true);
+
     }
 
     public void roll(int...rolls){
